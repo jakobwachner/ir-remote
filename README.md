8827b68f66e63aaff9bd75ad32dc52c7614c5f79
Anton <anton.mehnert@stud.hszg.de>
2026-06-29 21:24:37 +0200

    simpler WiFi/web test

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # ESP32 IR Remote Control
 
 
+
 Dieses Projekt soll eine universelle Infrarot-Fernbedienung auf Basis eines ESP32 werden.
 
 Ziel ist es, Infrarotsignale beliebiger Fernbedienungen aufzunehmen, zu speichern und später wiederzugeben. Die Bedienung soll über eine Weboberfläche erfolgen, die direkt auf dem ESP32 läuft. Nutzer sollen eigene virtuelle Fernbedienungen erstellen und gespeicherte Signale einzelnen Schaltflächen zuordnen können.
+
 
 
 ## Geplante Funktionen
@@ -17,6 +19,5 @@ Ziel ist es, Infrarotsignale beliebiger Fernbedienungen aufzunehmen, zu speicher
 
 ## Projektstatus
 
-erste Tests zur Inbetriebnahme des ESP32 werden durchgeführt.
-
+erste Tests zur Ansteuerung einer LED...
 
--- main.ino
+++ main.ino
@@ -4,34 +4,59 @@
 const char* ssid = "Wlan";
 const char* password = "admin";
 
+const int IR_PIN = 13;
 WebServer server(80);
 
 
+bool ledState = false;
+
+const char INDEX_HTML[] = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"><title>ESP32 IR</title></head>
+<body style="font-family:Arial;text-align:center;margin-top:50px;">
+<h2>ESP32 IR</h2>
+<button style="padding:20px;font-size:20px;" onclick="toggleLed()">LED umschalten</button>
+
+<!-- fct zum umschalten -->
+<script>
+function toggleLed(){
+  fetch('/toggle').then(r => r.text()).then(console.log);
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
 void handleRoot() {
-  server.send(200, "text/plain", "ESP32 server running");
+  server.send(200, "text/html", INDEX_HTML);
+}
+
+void handleToggle() {
+  ledState = !ledState;
+  digitalWrite(IR_PIN, ledState);
+  server.send(200, "text/plain", ledState ? "ON" : "OFF");
 }
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  
-  WiFi.begin(ssid, password);
+  pinMode(IR_PIN, OUTPUT);
+  digitalWrite(IR_PIN, LOW);
 
-  
-  Serial.print("connecting");
+  Serial.print("Verbinde mit WLAN");
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(250);
     Serial.print(".");
   }
 
-  Serial.println("\nWiFi verbunden!");
-  Serial.print("IP addresse: ");
+  Serial.println();
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
   server.on("/", handleRoot);
-  
+  server.on("/toggle", handleToggle);
   server.begin();
-  Serial.println("HTTP server wurde gestartet");
 }
 
 void loop() {
